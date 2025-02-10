023b18036d48b85bfc406daa6b65bc23bab442bb
leiriane <162614987+leirioli@users.noreply.github.com>
2025-02-10 19:14:15 -0300

    Add files via upload

--- backEnd/database/models.py
+++ backEnd/database/models.py
@@ -1,36 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
 
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:810502@localhost/espacosilmarabd'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 db = SQLAlchemy()
 
-class Usuario(db.Model):
-    __tablename__ = 'usuarios'  # Nome da tabela no banco de dados
-    id_usuario = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(30), unique=True, nullable=False)
-    senha = db.Column(db.String(255))
-
-    def __repr__(self):
-        return f'<Usuario {self.nome}>'
-
-class Servico(db.Model):
-    __tablename__ = 'servicos'  # Nome da tabela no banco de dados
-    id_servico = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome_servico = db.Column(db.String(50), nullable=False)
-    descricao = db.Column(db.String(150))
-    valor = db.Column(db.Numeric(10, 2), nullable=False)
-
-    def __repr__(self):
-        return f'<Servico {self.nome_servico}>'
-
+# Modelo da tabela Agendamentos
 class Agendamento(db.Model):
-    __tablename__ = 'agendamentos'  # Nome da tabela no banco de dados
     id_agendamento = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    id_servico = db.Column(db.Integer, db.ForeignKey('servicos.id_servico'))
-    id_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id_usuario'))
-    formato = db.Column(db.String(15))
-    estado = db.Column(db.String(20))
-    data_hora = db.Column(db.DateTime)
-    observacoes = db.Column(db.String(150))
+    id_servico = db.Column(db.Integer, db.ForeignKey('servicos.id_servico'), nullable=True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id_usuario'), nullable=True)
+    formato = db.Column(db.String(15), nullable=True)
+    estado = db.Column(db.String(20), nullable=True)
+    data_hora = db.Column(db.DateTime, nullable=True)
+    observacoes = db.Column(db.String(150), nullable=True)
 
-    def __repr__(self):
-        return f'<Agendamento {self.id_agendamento}>'
+# Modelo da tabela Servicos
+class Servico(db.Model):
+    id_servico = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(100), nullable=False)
+    descricao = db.Column(db.String(255), nullable=True)
+
+# Modelo da tabela Usuarios
+class Usuario(db.Model):
+    id_usuario = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
